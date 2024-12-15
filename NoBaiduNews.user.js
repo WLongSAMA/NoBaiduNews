c1580135fc08a4f9ea283f564f116c4e4c4c1776
WLong <a1010841065@126.com>
2024-12-15 19:06:30 +0800

    重构脚本

--- NoBaiduNews.user.js
+++ NoBaiduNews.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         NoBaiduNews
 // @namespace    https://github.com/WLongSAMA/NoBaiduNews
-// @version      0.5.2
-// @description  屏蔽百度和必应主页新闻推荐流，只保留网址导航功能。
+// @version      1.0.0
+// @description  屏蔽百度页新闻推荐流，只保留网址导航功能。
 // @author       WLong
 // @license      MIT
 // @match        *://www.baidu.com
@@ -18,60 +18,100 @@
 (function () {
     'use strict';
 
-    var n = 0;
-    var int = self.setInterval(function () {
-        //debugger;
-        if (document.title == "百度一下") { //百度移动端
-            var newslist = document.getElementsByClassName("blank-frame");
-            if (newslist[0] != null) {
-                newslist[0].parentNode.removeChild(newslist[0]); //移除新闻流
-                if (document.getElementById("foot").firstChild.nodeName == "DIV") {
-                    document.getElementById("foot").firstChild.remove(); //移除提交反馈
-                }
-            }
-            document.getElementById("ns-square-point").style.display = "none"; //隐藏右上角小红点
-            document.getElementById("ts-image-uploader-icon").style.display = "none"; //隐藏不可用的图片搜索按钮
-            //document.getElementsByClassName("voice call")[0].style.borderRight = "none"; //隐藏录音图标后面的分隔线
-            document.getElementsByClassName("square-banner-bgicon")[0].style.display = "none"; //隐藏顶部热播爆款好剧
+    function IsMobile() {
+        let regex = /iPhone|iPad|iPod|iOS|Android/i;
+        if (regex.test(navigator.userAgent)) {
+            return true;
+        } else {
+            return false;
+        };
+    }
 
-            document.getElementById("navs").remove(); //移除列表分隔行
-            document.getElementById("logo").style.paddingTop = "150px"; //优化LOGO和搜索框的显示位置
+    function Selector(selector) {
+        return document.querySelector(selector);
+    }
 
-            window.addEventListener("resize", function () {
-                document.getElementById("header").style.height = document.documentElement.clientHeight - document.getElementById("bottom").clientHeight - 10 + "px"; //修复卡片高度
-            }, false);
-            window.dispatchEvent(new Event('resize'));
-            window.clearInterval(int);
-        } else if (document.title == "百度一下，你就知道") { //百度桌面端
-            if (n >= 100) window.clearInterval(int);
-            if (document.getElementById("s_content_2")) {
-                document.getElementById("s_content_2").remove();
-            }
-            document.getElementById("s_menu_mine").click();
-            if (document.getElementById("s_content_100")) {
-                document.getElementById('s_menus_wrapper').innerHTML = '<span id="s_menu_mine" class="s-menu-item current" data-id="100"><span class="s-menu-item-underline"></span></span>';
-                window.clearInterval(int);
-            }
-            n++;
-        } else if (document.title == "搜索 - Microsoft 必应" || document.title == "Search - Microsoft Bing") { //必应
-            if (n >= 100) window.clearInterval(int);
+    function SelectorAll(selector) {
+        return document.querySelectorAll(selector);
+    }
 
-            document.getElementById("id_qrcode").style.display = "none"; //隐藏必应手机版图标
-            document.getElementById("id_qrcode_popup_positioner").style.display = "none"; //隐藏必应手机版二维码
-            document.getElementsByClassName("vs")[0].style.display = "none"; //隐藏新闻流
-            document.getElementsByClassName("caro_div")[0].style.display = "none"; //隐藏布局按钮
-            document.getElementsByClassName("scroll_cont show_partial")[0].style.top = "calc(100% - 3.25rem)"; //调整悬浮窗位置
-            document.getElementById("sb_feedback").style.display = "none"; //隐藏反馈按钮
+    addEventListener("load", (event) => {
+        let n = 0;
+        let interval;
+        if (!IsMobile()) {
+            if (document.title == "百度一下，你就知道") { //百度桌面端
+                interval = self.setInterval(function () {
+                    if (n >= 10) window.clearInterval(interval);
 
-            document.getElementById("footer").style.display = "none"; //隐藏页脚
-
-            var place = document.getElementsByClassName("sb_form_placeholder_query");
-            if (place.length > 0) {
-                place[0].style.display = "none"; //隐藏搜索框热词
-                window.clearInterval(int);
+                    let content = Selector("#s_content_2");
+                    if (content) {
+                        content.remove();
+                    }
+                    Selector("#s_menu_mine").click();
+                    if (Selector("#s_content_100")) {
+                        Selector("#s_menus_wrapper").innerHTML = '<span id="s_menu_mine" class="s-menu-item current" data-id="100"><span class="s-menu-item-underline"></span></span>';
+                        window.clearInterval(interval);
+                    }
+                    n++;
+                }, 1000);
             }
         } else {
-            window.clearInterval(int);
+            if (document.title == "百度一下") { //百度移动端
+                Selector(".blank-frame").remove(); //移除新闻流
+                Selector("#foot").firstChild.remove(); //移除用户反馈
+                Selector("#ns-square-point").style.display = "none"; //隐藏右上角小红点
+                Selector(".square-banner-bgicon").style.display = "none"; //隐藏顶部热播爆款好剧
+                Selector(".callicon-wrap").style.display = "none"; //隐藏图片搜索按钮和录音按钮
+
+                Selector("#navs").remove(); //移除列表分隔行
+                Selector("#logo").style.paddingTop = "150px"; //优化LOGO和搜索框的显示位置
+
+                window.addEventListener("resize", function () {
+                    Selector("#header").style.height = document.documentElement.clientHeight - Selector("#bottom").clientHeight - 10 + "px"; //修复卡片高度
+                }, false);
+                window.dispatchEvent(new Event('resize'));
+            }
         }
-    }, 1000);
+
+        //必应已经可以通过菜单关闭新闻流
+        /*
+        if (document.title == "搜索 - Microsoft 必应" || document.title == "Search - Microsoft Bing") { //必应
+            let qrcode = Selector("#id_qrcode");
+            if (qrcode) {
+                qrcode.style.display = "none"; //隐藏必应手机版图标
+            }
+            let qrcode_popup = Selector("#id_qrcode_popup_positioner");
+            if (qrcode_popup) {
+                qrcode_popup.style.display = "none"; //隐藏必应手机版二维码
+            }
+
+            let place = Selector(".sb_form_placeholder_query");
+            if (place) {
+                place.style.display = "none"; //隐藏搜索框热词
+            }
+
+            Selector(".vs").style.display = "none"; //隐藏新闻流
+
+            let feedback = Selector("#sb_feedback");
+            if (feedback) {
+                feedback.style.display = "none"; //隐藏反馈按钮
+            }
+
+            if (!IsMobile()) {
+                interval = self.setInterval(function () {
+                    Selector(".show_partial").style.top = "calc(100% - 3.25rem)"; //调整悬浮窗位置
+                    Selector(".caro_div").style.display = "none"; //隐藏布局按钮
+                    window.clearInterval(interval);
+                }, 1000);
+            } else {
+                Selector("#vs_cont").style.top = "100%"; //调整悬浮窗位置
+
+                let footer = Selector("#footer");
+                if (footer) {
+                    footer.style.display = "none"; //隐藏页脚
+                }
+            }
+        }
+        */
+    });
 })();
